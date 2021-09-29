2f0507e7ae20eb8263c8de5582e5e41c8a33c032
Jordan Sussman <52428171+jordan-sussman@users.noreply.github.com>
2021-09-29 14:03:27 -0700

    Added logic for track album artwork

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import json
+import webbrowser
+import time
 import requests
 
+from refresh import Refresh
 from secrets import spotify_user_id, discover_weekly_id
 from refresh import Refresh
 
@@ -12,6 +15,7 @@ class PlaylistSongs:
         self.tracks = ""
         self.new_playlist_id = ""
 
+# Get track metadata from Spotify API with designated playlist in secrets.py
     def find_songs(self):
         query = "https://api.spotify.com/v1/playlists/{}/tracks".format(
             discover_weekly_id)
@@ -19,8 +23,19 @@ class PlaylistSongs:
                                 headers={"Content-Type": "application/json",
                                          "Authorization": "Bearer {}".format(self.spotify_token)})
         response_json = response.json()
-        print(response_json)
+        print(response)
 
+# Loop through all tracks in playlist to grab each song's album artwork URL into a list. 
+        data = response_json
+        for x in data['tracks']['items']:
+            print(x['track']['album']['images'][0]['url'])
+
+# Open every track's album artwork into a browser tab with a small delay between each
+            urls = str(x['track']['album']['images'][0]['url'])
+            webbrowser.open(urls)
+            time.sleep(2)
+
+# Call to refresh Spotify authorization token
     def call_refresh(self):
         print("Refreshing token")
         refreshCaller = Refresh()
