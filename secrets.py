54c614cdfd60248c59d74dbf81c2c6d6be624338
Jordan Sussman <52428171+jordan-sussman@users.noreply.github.com>
2021-10-04 09:16:18 -0700

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,10 +1,5 @@
-import requests
 # To complete this, go to https://developer.spotify.com/documentation/general/guides/authorization-guide
-# This is your Spotify User ID
-spotify_user_id = ~
-# This is your Discover Weekly playlist ID
-discover_weekly_id = ~
-# This is the refresh token
-refresh_token = ~
-# This is your encoded Client ID and Client Secret
-base_64 = ~
+spotify_user_id = "YOUR SPOTIFY USER ID"
+discover_weekly_id = "YOUR SPOTIFY DISCOVER WEEKLY PLAYLIST ID"
+refresh_token = "YOUR REFRESH TOKEN FROM SPOTIFY"
+base_64 = "YOUR ENCODED CLIENT ID AND CLIENT SECRET FROM SPOTIFY"
